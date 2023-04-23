1e11d06fd38a75d217b238ad013ecdeb1864ef7a
Igor Malyarov <igor1309@gmail.com>
2023-04-23 14:57:29 +0300

    add app target

---
+++ Apps/SampleVMClientAPP/SampleVMClientAPP/SampleVMClientAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleVMClientAppApp.swift
+//  SampleVMClientApp
+//
+//  Created by Igor Malyarov on 23.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct SampleVMClientAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
