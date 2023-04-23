4034a3b29b03c4df4162cc558c50fb393ceeb476
Igor Malyarov <igor1309@gmail.com>
2023-04-23 13:42:19 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ContainThirdPartyDependencies
+
+A description of this package.
---
+++ Tests/ContainThirdPartyDependenciesTests/ContainThirdPartyDependenciesTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import ContainThirdPartyDependencies
+
+final class ContainThirdPartyDependenciesTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(ContainThirdPartyDependencies().text, "Hello, World!")
+    }
+}
